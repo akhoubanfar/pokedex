677aeb73af5d97cf10a20c78440918bab14c215d
akhouban <56620576+akhouban@users.noreply.github.com>
2021-07-06 21:06:13 +0800

    code update

--- pokemons/__test__/pokemon.test.ts
+++ pokemons/__test__/pokemon.test.ts
@@ -6,7 +6,7 @@ it("returns a 404 if the pokemon is not found", async () => {
   await request(app).get(`/pokemons/${name}`).send().expect(404);
 });
 
-it("returns the pokemon if the ticket is found", async () => {
+it("returns the pokemon if the pokemon is found", async () => {
   const name = "mewtwo";
 
   const response = await request(app)
@@ -15,4 +15,26 @@ it("returns the pokemon if the ticket is found", async () => {
     .expect(200);
 
   expect(response.body.name).toEqual(name);
+});
+
+it("returns the shakespeare translation if the pokemon is found", async () => {
+  const name = "mewtwo";
+
+  const response = await request(app)
+    .get(`/pokemons/translated/${name}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.description).toContain("yoda");
+});
+
+it("returns the shakespeare translation if the pokemon is found", async () => {
+  const name = "charmander";
+
+  const response = await request(app)
+    .get(`/pokemons/translated/${name}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.description).toContain("shakespeare");
 });
